4d4de499a49cc3eb36ccfea6b9d2b351cd30a78e
James Rooke <contactme765@proton.me>
2026-07-07 10:56:43 -0500

    navbar logo

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { VscCode } from "react-icons/vsc";
 
 function Logo() {
   return (
-    <div>
-      <h1>Logo</h1>
-    </div>
-  )
+    <Button size="icon" asChild>
+      <Link href="/">
+        <VscCode className="w-6 h-6" />
+      </Link>
+    </Button>
+  );
 }
 
-export default Logo
+export default Logo;
