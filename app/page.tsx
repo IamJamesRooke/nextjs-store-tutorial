7b2460789e48bb651aa5142ac0d6c83049052a26
James Rooke <contactme765@proton.me>
2026-07-07 09:18:56 -0500

    basic shadcn importation of components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button"
+
 function HomePage() {
   return (
     <div>
-      Home Page
+      <h1>Home Page</h1>
+      <Button variant={"default"} size={"lg"}className='capitalize m-8'>Click Me</Button>
     </div>
   )
 }
