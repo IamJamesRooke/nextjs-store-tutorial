d820ae2bff51f4007d6d7679b406f0ea43e65463
James Rooke <contactme765@proton.me>
2026-07-09 12:14:25 -0500

    dynamic search bar component

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,24 +1,27 @@
-import Container from '../global/Container'
-import CartButton from './CartButton'
-import Logo from './Logo'
-import NavSearch from './NavSearch'
-import DarkMode from './DarkMode'
-import LinksDropdown from './LinksDropdown'
+import Container from "../global/Container";
+import CartButton from "./CartButton";
+import Logo from "./Logo";
+import NavSearch from "./NavSearch";
+import DarkMode from "./DarkMode";
+import LinksDropdown from "./LinksDropdown";
+import { Suspense } from "react";
 
 function Navbar() {
   return (
     <nav className="border-b">
-        <Container className='flex flex-col sm:flex-row sm:justify-between sm:items-center flex-wrap py-8 gap-4'>
-            <Logo />
-            <NavSearch />
-            <div className="flex gap-4 items-center">
-                <CartButton />
-                <DarkMode />  
-                <LinksDropdown />
-            </div>
-        </Container>
+      <Container className="flex flex-col sm:flex-row sm:justify-between sm:items-center flex-wrap py-8 gap-4">
+        <Logo />
+        <Suspense>
+          <NavSearch />
+        </Suspense>
+        <div className="flex gap-4 items-center">
+          <CartButton />
+          <DarkMode />
+          <LinksDropdown />
+        </div>
+      </Container>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
