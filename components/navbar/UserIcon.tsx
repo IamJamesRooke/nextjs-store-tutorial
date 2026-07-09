ee49436e14db1380762487ceb87d2fd03555f69c
James Rooke <contactme765@proton.me>
2026-07-09 17:59:30 -0500

    user icon component

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,7 +1,24 @@
-import { UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import { LuUser2 } from "react-icons/lu";
+import { currentUser, auth } from "@clerk/nextjs/server";
 
-function UserIcon() {
-  return <UserButton />;
+async function UserIcon() {
+  // const {userId} = auth();
+  const user = await currentUser();
+  const profileImage = user?.imageUrl;
+
+  if (profileImage) {
+    return (
+      <Image
+        src={profileImage}
+        alt="User profile"
+        width={24}
+        height={24}
+        className="rounded-full object-cover"
+      />
+    );
+  }
+  return <LuUser2 className ='w-6 h-6 bg-primary rounded-full text-white'/>;
 }
 
 export default UserIcon;
