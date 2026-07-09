869a0f3ab7e398feacc7ee96a3dc11a96a3cbcab
James Rooke <contactme765@proton.me>
2026-07-09 08:50:52 -0500

    hero component

--- components/home/HeroCarousel.tsx
+++ components/home/HeroCarousel.tsx
@@ -1,4 +1,17 @@
-import React from 'react'
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/ui/carousel';
+import { Card, CardContent } from '@/components/ui/card';
+import Image from 'next/image';
+import hero1 from '@/public/images/hero1.jpg';
+import hero2 from '@/public/images/hero2.jpg';
+import hero3 from '@/public/images/hero3.jpg';
+import hero4 from '@/public/images/hero4.jpg';
+
 
 function HeroCarousel() {
   return (
