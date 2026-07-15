f1c9771c2f2c9e44fe7eac1d8c417b9f11ca4ff4
James Rooke <contactme765@proton.me>
2026-07-15 14:36:22 -0500

    react share library and social media buttons added

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,10 +1,11 @@
-import BreadCrumbs from '@/components/single-product/BreadCrumbs';
-import { fetchSingleProduct } from '@/utils/actions';
-import Image from 'next/image';
-import { formatCurrency } from '@/utils/format';
-import FavoriteToggleButton from '@/components/products/FavoriteToggleButton';
-import AddToCart from '@/components/single-product/AddToCart';
-import ProductRating from '@/components/single-product/ProductRating';
+import BreadCrumbs from "@/components/single-product/BreadCrumbs";
+import { fetchSingleProduct } from "@/utils/actions";
+import Image from "next/image";
+import { formatCurrency } from "@/utils/format";
+import FavoriteToggleButton from "@/components/products/FavoriteToggleButton";
+import AddToCart from "@/components/single-product/AddToCart";
+import ProductRating from "@/components/single-product/ProductRating";
+import ShareButton from "@/components/single-product/ShareButton";
 
 async function SingleProductPage({
   params,
@@ -14,35 +15,39 @@ async function SingleProductPage({
   const { id } = await params;
   const product = await fetchSingleProduct(id);
   const { name, image, company, description, price } = product;
-  const imagePath = image.startsWith('/') || image.startsWith('http') ? image : `/${image}`;
+  const imagePath =
+    image.startsWith("/") || image.startsWith("http") ? image : `/${image}`;
   const dollarsAmount = formatCurrency(price);
   return (
     <section>
       <BreadCrumbs name={product.name} />
-      <div className='mt-6 grid gap-y-8 lg:grid-cols-2 lg:gap-x-16'>
+      <div className="mt-6 grid gap-y-8 lg:grid-cols-2 lg:gap-x-16">
         {/* IMAGE FIRST COL */}
-        <div className='relative h-full'>
+        <div className="relative h-full">
           <Image
             src={imagePath}
             alt={name}
             fill
-            sizes='(max-width:768px) 100vw,(max-width:1200px) 50vw,33vw'
+            sizes="(max-width:768px) 100vw,(max-width:1200px) 50vw,33vw"
             priority
-            className='w-full rounded-md object-cover'
+            className="w-full rounded-md object-cover"
           />
         </div>
         {/* PRODUCT INFO SECOND COL */}
         <div>
-          <div className='flex gap-x-8 items-center'>
-            <h1 className='capitalize text-3xl font-bold'>{name}</h1>
-            <FavoriteToggleButton productId={id} />
+          <div className="flex gap-x-8 items-center">
+            <h1 className="capitalize text-3xl font-bold">{name}</h1>
+            <div className="flex items-center gap-x-2">
+              <FavoriteToggleButton productId={id} />
+              <ShareButton name={product.name} productId={params.id} />
+            </div>
           </div>
           <ProductRating productId={id} />
-          <h4 className='text-xl mt-2'>{company}</h4>
-          <p className='mt-3 text-md bg-muted inline-block p-2 rounded-md'>
+          <h4 className="text-xl mt-2">{company}</h4>
+          <p className="mt-3 text-md bg-muted inline-block p-2 rounded-md">
             {dollarsAmount}
           </p>
-          <p className='mt-6 leading-8 text-muted-foreground'>{description}</p>
+          <p className="mt-6 leading-8 text-muted-foreground">{description}</p>
           <AddToCart productId={id} />
         </div>
       </div>
--- components/global/LoadingTable.tsx
+++ components/global/LoadingTable.tsx
@@ -3,7 +3,7 @@ import { Skeleton } from "../ui/skeleton";
 function LoadingTable({ rows = 5 }: { rows?: number }) {
   const tableRows = Array.from({ length: rows }, (_, index) => {
     return (
-      <div className="mb-4" key={i}>
+      <div className="mb-4" key={index}>
         <Skeleton className="w-full h-8 rounded" />
       </div>
     );
